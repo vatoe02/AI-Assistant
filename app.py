aaf33d1bfded3642e0594a8d3c3560e39d2126cc
vatoe02 <vatoencube@gmail.com>
2024-08-08 04:55:31 +0200

    initial commit

--- app.py
+++ app.py
@@ -13,3 +13,21 @@ openai_key = os.getenv("OPENAI_API_KEY")
 openai_ef = embedding_functions.OpenAIEmbeddingFunction(
     api_key=openai_key, model_name="text-embedding-3-small"
 )
+
+chroma_client = chromadb.PersistentClient(path="chroma_persistent_storage")
+collection_name = "document_qa_collection"
+collection = chroma_client.get_or_create_collection(
+    name=collection_name, embedding_function=openai_ef
+)
+
+client = OpenAI(api_key=openai_key)
+
+resp = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role":"system","content":"You are a physics and chemistry helpful Assistant"},
+        {"role":"user","content":"what is the photoelectric effect?"}
+    ],
+)
+
+print(resp)
